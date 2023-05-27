03d3cc4fc9b576a801e6186c8c067217d1cb20b1
Niyas Salim <74399078+niyassaleem@users.noreply.github.com>
2023-05-27 17:09:27 +0530

    Add files via upload
    need to modify  the code to stop robot when the key is released

---
+++ cmd_vel/cmd_vel.ino
@@ -0,0 +1,89 @@
+/*
+   rosserial PubSub Example
+   Prints "hello world!" and toggles led
+*/
+
+#include <ros.h>
+#include <geometry_msgs/Twist.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Empty.h>
+
+char hello[13];
+ros::NodeHandle  nh;
+
+void callback_func(const geometry_msgs::Twist& cmdVel)
+{
+  if (cmdVel.linear.x > 0)
+  {
+    //hello[13] = "forward";
+    Serial.print("forward");
+    digitalWrite(6, 1);
+    digitalWrite(7, 0);
+    digitalWrite(8, 1);
+    digitalWrite(9, 0);
+    return 0;
+
+  }
+  if (cmdVel.linear.x < 0)
+  {
+    Serial.print("backward");
+    digitalWrite(6, 0);
+    digitalWrite(7, 1);
+    digitalWrite(8, 0);
+    digitalWrite(9, 1);
+    return 0;
+  }
+  if (cmdVel.angular.z > 0)
+  {
+    digitalWrite(6, 1);
+    digitalWrite(7, 0);
+    digitalWrite(8, 0);
+    digitalWrite(9, 1);
+    return 0;
+  }
+  if (cmdVel.angular.z < 0)
+  {
+    digitalWrite(6, 0);
+    digitalWrite(7, 1);
+    digitalWrite(8, 1);
+    digitalWrite(9, 0);
+    return 0;
+  }
+  return stop;
+}
+
+
+void stop()
+{
+  digitalWrite(6, 0);
+  digitalWrite(7, 0);
+  digitalWrite(8, 0);
+  digitalWrite(9, 0);
+}
+
+
+ros::Subscriber<geometry_msgs::Twist> sub("turtle1/cmd_vel", &callback_func );
+
+std_msgs::String str_msg;
+//ros::Publisher chatter("chatter", &str_msg);
+
+void setup()
+{
+  Serial.begin(115200);
+  nh.getHardware()->setBaud(115200);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  nh.initNode();
+  nh.subscribe(sub);
+  //  nh.advertise(chatter);
+}
+
+void loop()
+{
+  str_msg.data = hello;
+  //  chatter.publish( &str_msg );
+  nh.spinOnce();
+  delay(500);
+}
